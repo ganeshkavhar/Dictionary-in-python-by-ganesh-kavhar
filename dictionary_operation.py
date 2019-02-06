01004c27e31232cce1942ebf0f70966a3bd8a424
ganesh kavhar <kavharg@gmail.com>
2019-02-06 07:44:11 +0530

    Add files via upload

---
+++ dictionary_operation.py
@@ -0,0 +1,18 @@
+# Python program to demonstrate 
+# accesing a element from a Dictionary 
+
+# Creating a Dictionary 
+Dict = {1: 'Geeks', 'name': 'For', 3: 'Geeks'} 
+
+# accessing a element using key 
+print("Acessing a element using key:") 
+print(Dict['name']) 
+
+# accessing a element using key 
+print("Acessing a element using key:") 
+print(Dict[1]) 
+
+# accessing a element using get() 
+# method 
+print("Acessing a element using get:") 
+print(Dict.get(3)) 
